11df4ce6070c186f9e1989cd0c479e0b719b2cea
Alejo1120 <cedenoalejandro117@gmail.com>
2022-02-05 22:15:35 -0500

    formulario_infowhatsApp

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,34 +1,36 @@
-
-
-var nombre, email, phone, mensaje;
-
-
-
-
-async function enviaremail() {  
-  nombre = document.getElementById("nombre").value;
-  email = document.getElementById("email").value;
-  phone = document.getElementById("phone").value;
-  mensaje = document.getElementById("mensaje").value;
-
-  const data = {
-    nombre, email, phone, mensaje
-  }
-
-  console.log('nuestros datos',data);
-  
-  let response = await fetch('http://127.0.0.1:5000/enviar_correo', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(data)
-  });
-  
-
-let result = await response.json();
-alert(result.message);
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
 }
 
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '+573007747127';
 
-
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let apellidos = document.querySelector('#apellidos').value
+        let tipo=document.querySelector('#tipo').value
+        let email = document.querySelector('#email').value
+        let mensaje = 'send?phone=' + telefono + '&text=*_Formulario Enviado desde Tecnicenter WEB_*%0A*¿Cual es tu nombre y apellido?*%0A' + nombre + '%0A*DIreccion de donde sera el servicio:*%0A' + apellidos + '%0A*Tipo de electrodomestico y Marca {ejm: nevera o lavadora}*%0A' + tipo +'%0A*mensaje del daño del electrodomestico*%0A' + email + ''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
